d27b5f4321d05b3eb95ffd2902c356844ca29a67
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-18 06:01:16 -0700

    Fix error square a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 def square_matrix_simple(matrix=[]):
     row = len(matrix)
     col = len(matrix[0])
-    mat_sqr = [[0 for i in range(col)] for j in range(row)]
+    mat_sqr = [[0 for c in range(col)] for r in range(row)]
     for r in range(row):
         for c in range(col):
             mat_sqr[r][c] = matrix[r][c]**2
