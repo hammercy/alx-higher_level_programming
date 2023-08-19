0c6a2c56bf12d7687093083490a1057bca85e886
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-19 13:31:24 -0700

    create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[str.find(",", str.find(",")+1)+2:str.find(",", str.find(",")+1)+38] + "\n"
+print(str)
