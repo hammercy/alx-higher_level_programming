a928f6c8b9bf09ea576aebccb3096ad8e1f2e63b
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-19 14:36:24 -0700

    create new setence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find(",", str.find(",")+1)+2:str.find(",", str.find(",")+1)+38]
+str = " ".join([str.split()[5], str.split()[6], str.split()[7], str.split()[12], str.split()[0]])
 print(str)
