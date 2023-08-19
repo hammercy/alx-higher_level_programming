8762fc076b51cc8f658eab5db5945917811bc357
Fesseha Endrias <fessehaenha@gmail.com>
2023-08-19 13:37:43 -0700

    create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find(",", str.find(",")+1)+2:str.find(",", str.find(",")+1)+38] + "\n"
+str = str[str.find(",", str.find(",")+1)+2:str.find(",", str.find(",")+1)+38]
 print(str)
